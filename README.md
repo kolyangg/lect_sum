a0806f16bcf2336b98eefdbbc0f1e0ac982281c0
Nikolay <nasilaev@edu.hse.ru>
2025-02-25 22:42:51 +0000

    upd

---
+++ README.md
@@ -0,0 +1,43 @@
+# Speech enhancement
+Репозиторий с проектом по теме Youtube / Video lecture summarize
+
+
+ 
+# Использование кода
+
+```bash
+cd lect_summ
+```
+
+## 1. Настройка окружения
+
+```bash
+conda env create -f environment.yml
+
+```
+
+```bash
+git clone git@github.com:facebookresearch/segment-anything.git
+cd segment-anything; pip install -e .
+
+```
+
+# Если нет ffmpeg, для Ubuntu его нужно собрать (https://trac.ffmpeg.org/wiki/CompilationGuide/Ubuntu)
+```bash
+utils/ffmpeg_install.sh
+```
+
+
+## 2. Скачивание и подготовка данных
+### Пример YT video
+```bash
+yt-dlp https://www.youtube.com/watch?v=CSFjlhr_cz0
+mv "05. LLM Validation (04.02.25) [CSFjlhr_cz0].mkv" "testing_video.mkv"
+# ffmpeg -hwaccel cuda -i "testing_video.mkv" -c:v libvpx-vp9 -b:v 500k -vf "scale=1280:720" -c:a aac -b:a 64k "testing_video.webm"
+ffmpeg -i "testing_video.mkv" -c:v libvpx-vp9 -b:v 500k -vf "scale=1280:720" -c:a libvorbus -b:a 64k "testing_video.webm"
+
+
+```
+
+## 3. Извлечение кадров слайдов из видео
+
---
+++ utils/ffmpeg_install.sh
@@ -0,0 +1,129 @@
+# FFMPEG installation script
+
+
+# Source: https://trac.ffmpeg.org/wiki/CompilationGuide/Ubuntu
+
+# 1) Get dependencies
+sudo apt-get update -qq && sudo apt-get -y install \
+  autoconf \
+  automake \
+  build-essential \
+  cmake \
+  git-core \
+  libass-dev \
+  libfreetype6-dev \
+  libgnutls28-dev \
+  libmp3lame-dev \
+  libsdl2-dev \
+  libtool \
+  libva-dev \
+  libvdpau-dev \
+  libvorbis-dev \
+  libxcb1-dev \
+  libxcb-shm0-dev \
+  libxcb-xfixes0-dev \
+  meson \
+  ninja-build \
+  pkg-config \
+  texinfo \
+  wget \
+  yasm \
+  zlib1g-dev
+
+sudo apt install libunistring-dev libaom-dev libdav1d-dev
+
+mkdir -p ~/ffmpeg_sources ~/bin
+
+# 2) Install
+
+# a) NASM
+cd ~/ffmpeg_sources && \
+wget https://www.nasm.us/pub/nasm/releasebuilds/2.16.01/nasm-2.16.01.tar.bz2 && \
+tar xjvf nasm-2.16.01.tar.bz2 && \
+cd nasm-2.16.01 && \
+./autogen.sh && \
+PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" && \
+make && \
+make install
+
+# b) libx264
+cd ~/ffmpeg_sources && \
+git -C x264 pull 2> /dev/null || git clone --depth 1 https://code.videolan.org/videolan/x264.git && \
+cd x264 && \
+PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static --enable-pic && \
+PATH="$HOME/bin:$PATH" make && \
+make install
+
+# c) libx265
+sudo apt-get install libnuma-dev && \
+cd ~/ffmpeg_sources && \
+wget -O x265.tar.bz2 https://bitbucket.org/multicoreware/x265_git/get/master.tar.bz2 && \
+tar xjvf x265.tar.bz2 && \
+cd multicoreware*/build/linux && \
+PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED=off ../../source && \
+PATH="$HOME/bin:$PATH" make && \
+make install
+
+
+# d) libvpx
+cd ~/ffmpeg_sources && \
+git -C libvpx pull 2> /dev/null || git clone --depth 1 https://chromium.googlesource.com/webm/libvpx.git && \
+cd libvpx && \
+PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --disable-examples --disable-unit-tests --enable-vp9-highbitdepth --as=yasm && \
+PATH="$HOME/bin:$PATH" make && \
+make install
+
+# e) libopus
+cd ~/ffmpeg_sources && \
+git -C opus pull 2> /dev/null || git clone --depth 1 https://github.com/xiph/opus.git && \
+cd opus && \
+./autogen.sh && \
+./configure --prefix="$HOME/ffmpeg_build" --disable-shared && \
+make && \
+make install
+
+
+
+# f) libfdk-aac
+cd ~/ffmpeg_sources && \
+git -C fdk-aac pull 2> /dev/null || git clone --depth 1 https://github.com/mstorsjo/fdk-aac && \
+cd fdk-aac && \
+autoreconf -fiv && \
+./configure --prefix="$HOME/ffmpeg_build" --disable-shared && \
+make && \
+make install
+
+
+# 3) Final install
+
+
+cd ~/ffmpeg_sources && \
+wget -O ffmpeg-snapshot.tar.bz2 https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2 && \
+tar xjvf ffmpeg-snapshot.tar.bz2 && \
+cd ffmpeg && \
+PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure \
+  --prefix="$HOME/ffmpeg_build" \
+  --pkg-config-flags="--static" \
+  --extra-cflags="-I$HOME/ffmpeg_build/include" \
+  --extra-ldflags="-L$HOME/ffmpeg_build/lib" \
+  --extra-libs="-lpthread -lm" \
+  --ld="g++" \
+  --bindir="$HOME/bin" \
+  --enable-gpl \
+  --enable-gnutls \
+  --enable-libass \
+  --enable-libfdk-aac \
+  --enable-libfreetype \
+  --enable-libmp3lame \
+  --enable-libvorbis \
+  --enable-libvpx \
+  --enable-libx264 \
+  --enable-libx265 \
+  --enable-nonfree && \
+PATH="$HOME/bin:$PATH" make && \
+make install && \
+hash -r
+
+
+# 4) Reogin to recognize new ffmpeg location
+source ~/.profile
